5a251dd4e89d059f3d38210d1512cb8d1d535c8d
IanA <iannaianjos@gmail.com>
2024-08-02 21:50:31 -0300

    Version: 000.000.007 - 02-08-2024 - 21:50

--- card_game.py
+++ card_game.py
@@ -166,7 +166,7 @@ if __name__ == "__main__":
     TIMES = [[CARTAS[aleatorios[0]].copy(),
               CARTAS[aleatorios[1]].copy(),
               CARTAS[aleatorios[2]].copy()],
-             [CARTAS["duas_faces"].copy(),
+             [CARTAS["morcego"].copy(),
               CARTAS["vinganca_da_noite"].copy(),
               CARTAS["fenix"].copy()]]
 
