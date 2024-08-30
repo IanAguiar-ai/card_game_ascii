5ec74cbb3a55df4741210722c901d3c9ea6c4861
ian <ian.anjos@onerf.com.br>
2024-08-30 09:53:43 -0300

    Version: 000.000.021 - 30-08-2024 - 09:53

--- card_builder.py
+++ card_builder.py
@@ -8,8 +8,9 @@ from game_config import *
 from arts import *
 from auxiliary_functions import *
 from pure_engine_ascii import Screen
+from engine_card_game import raridades, classes
 
-def card_builder(itens:list):
+def card_builder():
     """
     Sistema de construção de carta
 
@@ -28,120 +29,69 @@ def card_builder(itens:list):
     if memoria_save == None:
         memoria_save = criar_save()
 
-    pos_caixa = 0
+    classes, index_classes = tuple(globals()["classes"].keys()), 0
+    raridades, index_raridades = tuple(globals()["raridades"].keys()), 0
+
+    carta = {"nome":"???",
+             "hp":999,
+             "preco":1,
+             "classe":classes[index_classes],
+             "arte":janela,
+             "raridade":raridades[index_raridades],
+             "ataques":[]
+             }
     
     clear_all()
-    game = Screen(x = X, y = Y, fps = FPS_LOJA)
+    game = Screen(x = X, y = Y, fps = 1)
     
     game_t = Thread(target = game.run)
     game_t.start()
 
     while True:
-        try:
-            texto_principal = f"MOEDAS: \033[93m{memoria_save['moedas']}\033[0m\n\nAperte:\n(A, W, S, D) Para andar entre os itens\n(C) Para comprar {itens[pos_caixa]['nome'].lower()}"
-        except:
-            texto_principal = f"MOEDAS: \033[93m{memoria_save['moedas']}\033[0m\n\nAperte:\n(A, W, S, D) Para andar entre os itens\n(C) Para comprar"
+        texto_principal = f"Use as teclas para iteragir"
         game.buffer_text = texto_principal
+
+        x_carta = 105
+        y_carta = 0
         
-        game.add_effects(x = 85, y = 0,
-                         image = estante,
+        game.add_effects(x = x_carta, y = y_carta,
+                         image = base_card_complete,
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 95, y = 22,
-                         image = teia_de_aranha,
+        game.add_effects(x = x_carta + 1, y = y_carta + 19,
+                         image = [*put_color_rarity([list(f"{raridades[index_raridades].title().center(34,'=')}")], rarity = raridades[index_raridades])],
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
+##
+##        self.add_temporary(Element(x = x_ + 1, y = y_ + 19, image = [*put_color_rarity([list(f"{self.TIMES[y__][x__]['raridade'].title().center(34,'=')}")], rarity = self.TIMES[y__][x__]['raridade'])]))
+##        self.add_temporary(Element(x = x_ + 1, y = y_ + 19, image = [*put_color_rarity([list(f"{self.TIMES[y__][x__]['raridade'].title().center(34,'=')}")], rarity = self.TIMES[y__][x__]['raridade'])]))
+##        self.add_temporary(Element(x = x_ + 5, y = y_ + 1, image = [*put_color_class([list(f"{self.TIMES[y__][x__]['classe'].title().center(23)}")], class_ = self.TIMES[y__][x__]['classe'])]))
+##        self.add_temporary(Element(x = x_ + 29, y = y_ + 1, image = [list("HP:")]))
+##        self.add_temporary(Element(x = x_ + 32, y = y_ + 1, image = [*put_color_life([list(f"{self.TIMES[y__][x__]['hp_temp']:3}")], life = self.TIMES[y__][x__]['hp_temp'])]))
+##        self.add_temporary(Element(x = x_ + 1, y = y_ + 18, image = [list(f"{self.TIMES[y__][x__]['nome'].center(34)}")]))
+##        self.add_temporary(Element(x = x_ + 2, y = y_ + 1, image = [*put_color_rarity([list(f"({self.TIMES[y__][x__]['preco']})")], rarity = self.TIMES[y__][x__]['raridade'])]))
+##        pos = 21
+##        for t in self.TIMES[y__][x__]["ataques"]:
+##            if t["tipo"] == "ataque":
+##                self.add_temporary(Element(x = x_ + 2, y = y_ + pos, image = put_color_tipo([list(f"{t['nome']} ({t['dado']}) ({t['tipo'].title()})")], tipo = t['tipo'])))
+##                descricao = ajustar_descricao(t["descricao"])
+##                self.add_temporary(Element(x = x_ + 2, y = y_ + pos + 2, image = descricao))
+##            else:
+##                self.add_temporary(Element(x = x_ + 2, y = y_ + pos, image = put_color_tipo([list(f"{t['nome']} ({t['tipo'].title()})")], tipo = t['tipo'])))
+##                descricao = ajustar_descricao(t["descricao"])
+##                self.add_temporary(Element(x = x_ + 2, y = y_ + pos + 2, image = descricao))
+##            pos += 3 + len(descricao)
 
-        game.add_effects(x = 1, y = 0,
-                         image = teia_de_aranha_3,
-                         frames = 1,
-                         tipe = None,
-                         wait = 0,
-                         to_start = 0)
-
-        game.add_effects(x = 105, y = 20,
-                         image = poutrona,
-                         frames = 1,
-                         tipe = None,
-                         wait = 0,
-                         to_start = 0)
-
-        for x in range(2):
-            for y in range(2):
-                if pos_caixa == x + y*2:
-                    game.add_effects(x = 14 + x*36, y = 6 + y*16,
-                                 image = caixa_maior,
-                                 frames = 1,
-                                 tipe = None,
-                                 wait = 0,
-                                 to_start = 0)
-                
-                game.add_effects(x = 15 + x*36, y = 7 + y*16,
-                                 image = caixa_simples,
-                                 frames = 1,
-                                 tipe = None,
-                                 wait = 0,
-                                 to_start = 0)
-
-                if len(itens) > x + y*2:
-                    game.add_effects(x = 16 + x*36, y = 8 + y*16,
-                                     image = itens[x+y*2]["imagem"],
-                                     frames = 1,
-                                     tipe = None,
-                                     wait = 0,
-                                     to_start = 0)
-
-                    preco_ = list("$" + str(itens[x+y*2]["preco"]))
-                    preco_[0] = "\033[93m" + preco_[0]
-                    preco_[-1] = preco_[-1] + "\033[0m"
-                    game.add_effects(x = 20 + x*36, y = 14 + y*16,
-                                     image = [preco_],
-                                     frames = 1,
-                                     tipe = None,
-                                     wait = 0,
-                                     to_start = 0)
-
-                    if itens[x+y*2]["id"] in memoria_save["inventario"]:
-                        game.add_effects(x = 16 + x*36, y = 8 + y*16,
-                                     image = caixa_simples_fechada,
-                                     frames = 1,
-                                     tipe = None,
-                                     wait = 0,
-                                     to_start = 0)
-
-        if "descricao" in itens[pos_caixa]:
-            game.add_effects(x = 4, y = 32,
-                             image = caixa_texto(itens[pos_caixa]["descricao"], limite = 70),
-                             frames = 1,
-                             tipe = None,
-                             wait = 0,
-                             to_start = 0)
-                    
         resp = input()
-        if resp.lower() == "w":
-            pos_caixa = max(0, pos_caixa - 2)
-        elif resp.lower() == "a" or resp.lower() == "q":
-            pos_caixa = max(0, pos_caixa - 1)
-        elif resp.lower() == "s":
-            pos_caixa = min(3, pos_caixa + 2)
-        elif resp.lower() == "d" or resp.lower() == "e":
-            pos_caixa = min(3, pos_caixa + 1)
-        elif resp.lower() == "c":
-            if len(itens) > pos_caixa and memoria_save["moedas"] >= itens[pos_caixa]["preco"] and not itens[pos_caixa]["id"] in memoria_save["inventario"]:
-                memoria_save["inventario"].append(itens[pos_caixa]["id"])
-                memoria_save["moedas"] -= itens[pos_caixa]["preco"]
-                adicionar_save(memoria_save)
+
 
     game_t.join()
     game.close()
 
 if __name__ == "__main__":
-    store(itens = [itens["pocao_pequena"],
-                   itens["torta"],
-                   itens["drink"],
-                   itens["biscoito"]])
+    card_builder()
