fb69acfe2053eae76574121bc758a87d50902959
IanA <iannaianjos@gmail.com>
2024-11-29 11:08:24 -0300

    Version: 000.000.027 - 29-11-2024 - 11:08

--- map.py
+++ map.py
@@ -15,9 +15,36 @@ from pure_engine_ascii import Screen
 from translator import translate
 from text_mission import conferir_missoes
 
+class Selecionar:
+    def __init__(self, pos:tuple):
+        self.pos:tuple = pos
+        self.r = None
+        self.l = None
+        self.u = None
+        self.d = None
+
 def animacao_mapa(game, memoria:dict, debug:bool = True) -> None:
     pos_ondas = [(1, 1), (10, 2), (20, 10), (1, 14), (112, 1), (122, 5), (102, 7), (102, 32), (125, 40), (80, 40),
                  (55, 42), (10, 12), (112, 6), (132, 1), (135, 7), (91, 36), (68, 40), (114, 37), (135, 38)]
+
+    pos_locais = {"mapa_sol": {"x":130, "y":3},
+                  "mapa_lua": {"x":130, "y":3},
+                  "mapa_piramide": {"x":55, "y":20},
+                  "mapa_farol": {"x":92, "y":0},
+                  "mapa_castelo": {"x":28, "y":0},
+                  "mapa_castelo_2": {"x":45, "y":0},
+                  "mapa_cidade": {"x":73, "y":30},
+                  "mapa_fazenda": {"x":40, "y":33},
+                  "mapa_trem": {"x":18, "y":22},
+                  "mapa_montanha": {"x":58, "y":5},
+                  "mapa_boneco_de_neve": {"x":47, "y":8},
+                  "mapa_maquina_escavar": {"x":32, "y":26},
+                  "mapa_pegasus": {"x":86, "y":21},
+                  "mapa_vulcao": {"x":122, "y":26},
+                  "mapa_navio": {"x":5, "y":11},
+                  "mapa_espaconave": {"x":12, "y":30},
+                  "mapa_castelo_voador": {"x":100, "y":33},
+                  "mapa_ovni": {"x":136, "y":14},}
 
     #Nuvens
     pos_nuvem = []
@@ -33,6 +60,21 @@ def animacao_mapa(game, memoria:dict, debug:bool = True) -> None:
         com_sol = True
     else:
         com_sol = False
+
+    if "terra_a_vista" in memoria["missoes"]:
+        terra_a_vista = True
+    else:
+        terra_a_vista = False
+
+    if "ovni" in memoria["missoes"]:
+        ovni_ = True
+    else:
+        ovni_ = False
+
+    if "castelo_flutuante" in memoria["missoes"]:
+        castelo_flutuante = True
+    else:
+        castelo_flutuante = False
 
     conferir_missoes(tipo = "mapa", save = memoria, tipo_clima = tipo_clima, com_sol = com_sol)
 
@@ -66,43 +108,43 @@ def animacao_mapa(game, memoria:dict, debug:bool = True) -> None:
                 com_sol = False
 
         if com_sol:
-            game.add_effects(x = 130, y = 3,
+            game.add_effects(x = pos_locais["mapa_sol"]["x"], y = pos_locais["mapa_sol"]["y"],
                              image = mapa_sol,
                              frames = 1,
                              tipe = None,
                              wait = 0,
                              to_start = 0)
         else:
-            game.add_effects(x = 130, y = 3,
+            game.add_effects(x = pos_locais["mapa_lua"]["x"], y = pos_locais["mapa_sol"]["y"],
                              image = mapa_lua,
                              frames = 1,
                              tipe = None,
                              wait = 0,
-                             to_start = 0)        
+                             to_start = 0)
 
         #Monumentos ===================================================
-        game.add_effects(x = 55, y = 20,
+        game.add_effects(x = pos_locais["mapa_piramide"]["x"], y = pos_locais["mapa_piramide"]["y"],
                          image = mapa_piramide,
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 92, y = 0,
+        game.add_effects(x = pos_locais["mapa_farol"]["x"], y = pos_locais["mapa_farol"]["y"],
                          image = mapa_farol,
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 28, y = 0,
+        game.add_effects(x = pos_locais["mapa_castelo"]["x"], y = pos_locais["mapa_castelo"]["y"],
                          image = mapa_castelo,
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 45, y = 0,
+        game.add_effects(x = pos_locais["mapa_castelo_2"]["x"], y = pos_locais["mapa_castelo_2"]["y"],
                          image = mapa_castelo_2,
                          frames = 1,
                          tipe = None,
@@ -116,7 +158,7 @@ def animacao_mapa(game, memoria:dict, debug:bool = True) -> None:
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 73, y = 30,
+        game.add_effects(x = pos_locais["mapa_cidade"]["x"], y = pos_locais["mapa_cidade"]["y"],
                          image = mapa_cidade_2,
                          frames = 1,
                          tipe = None,
@@ -130,73 +172,73 @@ def animacao_mapa(game, memoria:dict, debug:bool = True) -> None:
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 40, y = 33,
+        game.add_effects(x = pos_locais["mapa_fazenda"]["x"], y = pos_locais["mapa_fazenda"]["y"],
                          image = mapa_fazenda,
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 18, y = 22,
+        game.add_effects(x = pos_locais["mapa_trem"]["x"], y = pos_locais["mapa_trem"]["y"],
                          image = mapa_trem,
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 58, y = 5,
+        game.add_effects(x = pos_locais["mapa_montanha"]["x"], y = pos_locais["mapa_montanha"]["y"],
                          image = mapa_montanha,
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 47, y = 8,
+        game.add_effects(x = pos_locais["mapa_boneco_de_neve"]["x"], y = pos_locais["mapa_boneco_de_neve"]["y"],
                          image = mapa_boneco_de_neve,
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 32, y = 26,
+        game.add_effects(x = pos_locais["mapa_maquina_escavar"]["x"], y = pos_locais["mapa_maquina_escavar"]["y"],
                          image = mapa_maquina_escavar,
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 86, y = 21,
+        game.add_effects(x = pos_locais["mapa_pegasus"]["x"], y = pos_locais["mapa_pegasus"]["y"],
                          image = mapa_pegasus,
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
 
-        game.add_effects(x = 122, y = 26,
+        game.add_effects(x = pos_locais["mapa_vulcao"]["x"], y = pos_locais["mapa_vulcao"]["y"],
                          image = mapa_vulcao,
                          frames = 1,
                          tipe = None,
                          wait = 0,
                          to_start = 0)
     
-        if "terra_a_vista" in memoria["missoes"] or debug:
-            game.add_effects(x = 5, y = 11,
+        if terra_a_vista or debug:
+            game.add_effects(x = pos_locais["mapa_navio"]["x"], y = pos_locais["mapa_navio"]["y"],
                              image = mapa_navio,
                              frames = 1,
                              tipe = None,
                              wait = 0,
                              to_start = 0)
 
-        if "ovni" in memoria["missoes"] or debug:
-            game.add_effects(x = 12, y = 30,
+        if ovni_ or debug:
+            game.add_effects(x = pos_locais["mapa_ovni"]["x"], y = pos_locais["mapa_ovni"]["y"],
                              image = mapa_espaconave,
                              frames = 1,
                              tipe = None,
                              wait = 0,
                              to_start = 0)
 
-        if "castelo_flutuante" in memoria["missoes"] or debug:
-            game.add_effects(x = 100, y = 33,
+        if castelo_flutuante or debug:
+            game.add_effects(x = pos_locais["mapa_castelo_voador"]["x"], y = pos_locais["mapa_castelo_voador"]["y"],
                              image = mapa_castelo_voador,
                              frames = 1,
                              tipe = None,
@@ -205,7 +247,7 @@ def animacao_mapa(game, memoria:dict, debug:bool = True) -> None:
 
 
         if (ovni and not com_sol) or debug:
-            game.add_effects(x = 136, y = 14,
+            game.add_effects(x = pos_locais["mapa_ovni"]["x"], y = pos_locais["mapa_ovni"]["y"],
                              image = mapa_ovni,
                              frames = 1,
                              tipe = None,
